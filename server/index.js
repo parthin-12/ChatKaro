e6887a364e7356ed0a87b0dc069079f640c274f6
parthin-12 <201901113@daiict.ac.in>
2022-06-30 23:08:46 +0530

    push

--- server/index.js
+++ server/index.js
@@ -27,9 +27,9 @@ app.get("/api/v1/",(req,res)=>{
     res.send("HELL ITS WORKING");
 })
 
-app.use(express.static(path.join(__dirname,"../frontend/build")));
+app.use(express.static(path.join(__dirname,"../chartkaro.com/build")));
 app.get("*",(req,res)=>(
-    res.sendFile(path.resolve(__dirname,"../frontend/build/index.html"))
+    res.sendFile(path.resolve(__dirname,"../chartkaro.com/build/index.html"))
 ))
 
 const server=http.createServer(app);
